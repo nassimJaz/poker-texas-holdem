1e5530096ce21ecd22797ab4277d9978bbbfe32a
Nassim <nassim.jazouli@epfedu.fr>
2026-02-07 18:20:18 +0100

    feat(table) : ajout de la table de jeu

---
+++ src/main/java/com/excilys/kataspoker/Table.java
@@ -0,0 +1,58 @@
+package com.excilys.kataspoker;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Table {
+
+    private List<Joueur> joueurs = new ArrayList<Joueur>();
+    private int nbJoueurs = 0;
+    private Paquet paquet = new Paquet();
+
+    private List<Carte> board = new ArrayList<Carte>();
+
+    public Table(List<Joueur> joueurs) {
+        this.joueurs = joueurs;
+        this.nbJoueurs = this.joueurs.size();
+    }
+
+    public Joueur getJoueur(int indexJoueur) {
+        /* Récupère le joueur en rebouclant automatiquement (avec le modulo)
+         * si l'index dépasse de la liste */
+        int index = indexJoueur % nbJoueurs;
+        return joueurs.get(index);
+    }
+
+    public Joueur getJoueurSuivant(int indexJoueur) {
+        // Avance d'un joueur modulo appliqué
+        int indexSuivant = (indexJoueur + 1) % nbJoueurs;
+        return joueurs.get(indexSuivant);
+    }
+
+    public Joueur getJoueurSuivant(int indexJoueur, int decalage) {
+        int index = (indexJoueur + decalage) % nbJoueurs;
+        return joueurs.get(index);
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            Carte c = paquet.piocher();
+            board.add(c);
+        }
+    }
+
+    public void initialiserManche() {
+        paquet.shuffle();
+        for (int i = 0; i < 3*this.nbJoueurs; i++) {
+            Carte carte = paquet.piocher();
+            this.getJoueur(i).getHand().addHand(carte);
+        }
+        initializeBoard();
+    }
+
+
+
+
+
+}
