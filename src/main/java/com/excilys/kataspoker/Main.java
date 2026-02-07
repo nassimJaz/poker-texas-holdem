7e3be2cf915b9c669fd0dafc9991a6ae3aaf352f
Nassim <nassim.jazouli@epfedu.fr>
2026-02-07 18:31:12 +0100

    main

--- src/main/java/com/excilys/kataspoker/Main.java
+++ src/main/java/com/excilys/kataspoker/Main.java
@@ -1,10 +1,13 @@
 package com.excilys.kataspoker;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        /*
         Paquet paquet = new Paquet();
 
         //paquet.afficher();
@@ -16,6 +19,25 @@ public class Main {
         List<Carte> pioches = paquet.piocher(3);
         main.addHand(pioches);
         main.afficher();
+         */
+
+        Scanner sc = new Scanner(System.in);
+        List<Joueur> joueurstest = new ArrayList<Joueur>();
+        for (int i = 0; i < 3; i++) {
+           System.out.println("Le pseudo du joueur [" + i + "] : ");
+           String pseudo = sc.nextLine();
+           joueurstest.add(new Joueur(pseudo));
+        }
+        Table table = new Table(joueurstest);
+
+        table.initialiserManche();
+
+        for (int i = 0; i < joueurstest.size(); i++) {
+            System.out.println("[" + i + "] Cartes du Joueur : " + joueurstest.get(i).getPseudo());
+            joueurstest.get(i).getHand().afficher();
+        }
+
+        
 
     }
 }
